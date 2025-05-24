8f8d11e6e6cea7b8f0a14c018dffa75517bb4013
Mykhailo Levko <mykhailo.levko@gmail.com>
2025-05-24 12:03:08 -0400

    Added a CustomGlobalExceptionHandler class; meaningful validation for BookDTO class; hibernate validator dependency.

--- src/main/java/mate/academy/bookshopapp/dto/BookDto.java
+++ src/main/java/mate/academy/bookshopapp/dto/BookDto.java
@@ -1,15 +1,22 @@
 package mate.academy.bookshopapp.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class BookDto {
-    private Long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
