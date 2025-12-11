d82e5939a31e64567df0a7add4630fc5147102e3
Mykhailo Levko <mykhailo.levko@gmail.com>
2025-12-11 14:03:49 -0500

    Changed List<BookDto> to Page<BookDto>

--- src/main/java/mate/academy/bookshopapp/controller/BookController.java
+++ src/main/java/mate/academy/bookshopapp/controller/BookController.java
@@ -3,11 +3,11 @@ package mate.academy.bookshopapp.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookshopapp.dto.BookDto;
 import mate.academy.bookshopapp.dto.CreateBookRequestDto;
 import mate.academy.bookshopapp.service.BookService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -36,7 +36,7 @@ public class BookController {
 
     @GetMapping
     @Operation(summary = "Get all books", description = "Get a list of all available books")
-    public List<BookDto> getAll(Pageable pageable) {
+    public Page<BookDto> getAll(Pageable pageable) {
         return bookService.getAll(pageable);
     }
 
